1ad34984be46878e05d4d0053632b00e2716acbb
guiiiividd <guiiiividd@gmail.com>
2024-06-26 14:04:58 -0300

    Modificando o Endpoint GET /product

--- product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
+++ product-backend/src/main/java/com/abutua/product_backend/resources/ProductController.java
@@ -1,13 +1,20 @@
 package com.abutua.product_backend.resources;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.abutua.product_backend.models.Product;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class ProductController {
     
     @GetMapping("product")
-    public String getProduct(){
-        return "Eu sou o produto!";
+    public Product getProduct(){
+        Product p = new Product();
+        p.setId(1);
+        p.setName("Nome do Produto");
+        p.setPrice(100);
+        return p;
     }
 }
